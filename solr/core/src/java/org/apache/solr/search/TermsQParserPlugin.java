568d5012ad5d187f833cfc876f9e764ad2347a64
Jason Gerlowski <jason.gerlowski@lucidworks.com>
2019-10-23 10:35:21 -0400

    Fix post-backport compilation issue

--- solr/core/src/java/org/apache/solr/search/TermsQParserPlugin.java
+++ solr/core/src/java/org/apache/solr/search/TermsQParserPlugin.java
@@ -36,7 +36,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.LeafCollector;
 import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.Scorable;
+import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.TermInSetQuery;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.util.ArrayUtil;
@@ -295,7 +295,7 @@ public class TermsQParserPlugin extends QParserPlugin {
 
     private LeafCollector leafCollector;
 
-    public void setScorer(Scorable scorer) throws IOException {
+    public void setScorer(Scorer scorer) throws IOException {
       leafCollector.setScorer(scorer);
     }
 
@@ -397,7 +397,7 @@ public class TermsQParserPlugin extends QParserPlugin {
 
     private LeafCollector leafCollector;
 
-    public void setScorer(Scorable scorer) throws IOException {
+    public void setScorer(Scorer scorer) throws IOException {
       leafCollector.setScorer(scorer);
     }
 
