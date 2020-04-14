3708ce4ec789389fa867c3687a713fdcbb7ebcee
Joel Bernstein <jbernste@apache.org>
2020-04-14 11:49:46 -0400

    Eliminate facet refinements

--- solr/core/src/java/org/apache/solr/handler/component/FacetComponent.java
+++ solr/core/src/java/org/apache/solr/handler/component/FacetComponent.java
@@ -772,7 +772,7 @@ public class FacetComponent extends SearchComponent {
     //
     for (DistribFieldFacet dff : fi.facets.values()) {
       // no need to check these facets for refinement
-      if (dff.initialLimit <= 0 && dff.initialMincount <= 1) continue;
+      if (true || (dff.initialLimit <= 0 && dff.initialMincount <= 1)) continue;
 
       // only other case where index-sort doesn't need refinement is if minCount==0
       if (dff.minCount <= 1 && dff.sort.equals(FacetParams.FACET_SORT_INDEX)) continue;
